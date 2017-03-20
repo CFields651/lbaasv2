fa7cd018a5ce0e1b305a15cc2a5b81cf15c3d907
root <root@overcloud-controller-2.localdomain>
2017-03-20 15:05:28 +0000

    piped output to /tmp/lbaasv2.out

--- lbaasv2.sh
+++ lbaasv2.sh
@@ -1,8 +1,8 @@
 match="overcloud-controller"
 if echo $(hostname) | grep -q ^$match; then 
-  echo "$(hostname) macthes $match so installing lbaasv2" 
+  echo "$(hostname) macthes $match so installing lbaasv2" > /tmp/lbaasv2.out
 else 
-  echo "hostname $(hostname) does not match $match so exiting"
+  echo "hostname $(hostname) does not match $match so exiting" > /tmp/lbaasv2.out
   exit 0
 fi
 
@@ -27,21 +27,21 @@ crudini --set /etc/neutron/lbaas_agent.ini DEFAULT device_driver neutron_lbaas.d
 systemctl restart neutron-lbaasv2-agent.service
 systemctl enable neutron-lbaasv2-agent.service
 
-echo "---Results---"
+echo "---Results---" >> /tmp/lbaasv2.out
 #where neutron-server is running
-echo "neutron_lbaas.conf service_provider=$(crudini --get /etc/neutron/neutron_lbaas.conf service_providers service_provider)"
+echo "neutron_lbaas.conf service_provider=$(crudini --get /etc/neutron/neutron_lbaas.conf service_providers service_provider)" >> /tmp/lbaasv2.out
 plugins=$(crudini --get /etc/neutron/neutron.conf DEFAULT service_plugins)
-echo "neutron.conf service_plugins=$(crudini --get /etc/neutron/neutron.conf DEFAULT service_plugins)"
+echo "neutron.conf service_plugins=$(crudini --get /etc/neutron/neutron.conf DEFAULT service_plugins)" >> /tmp/lbaasv2.out
 
 #where lbaas agent is running 
-echo "lbaas_agent.ini device_driver=$(crudini --get /etc/neutron/lbaas_agent.ini DEFAULT device_driver)"
-echo "lbaas_agent.ini user_group=$(crudini --get /etc/neutron/lbaas_agent.ini DEFAULT user_group )"
-echo "lbaas_agent.ini interface_driver=$(crudini --get /etc/neutron/lbaas_agent.ini DEFAULT interface_driver)"
-echo "lbaas_agent.ini device_driver=$(crudini --get /etc/neutron/lbaas_agent.ini DEFAULT device_driver)" 
+echo "lbaas_agent.ini device_driver=$(crudini --get /etc/neutron/lbaas_agent.ini DEFAULT device_driver)" >> /tmp/lbaasv2.out
+echo "lbaas_agent.ini user_group=$(crudini --get /etc/neutron/lbaas_agent.ini DEFAULT user_group )" >> /tmp/lbaasv2.out
+echo "lbaas_agent.ini interface_driver=$(crudini --get /etc/neutron/lbaas_agent.ini DEFAULT interface_driver)" >> /tmp/lbaasv2.out
+echo "lbaas_agent.ini device_driver=$(crudini --get /etc/neutron/lbaas_agent.ini DEFAULT device_driver)" >> /tmp/lbaasv2.out
 
-echo "neutron-server $(systemctl is-active neutron-server)"
-echo "neutron-lbaasv2-agent $(systemctl is-active neutron-lbaasv2-agent)"
-
+echo "neutron-server $(systemctl is-active neutron-server)" >> /tmp/lbaasv2.out
+echo "neutron-lbaasv2-agent $(systemctl is-active neutron-lbaasv2-agent)" >> /tmp/lbaasv2.out
+cat /tmp/lbaasv2.out
 
 
 
